9b4cb56a4471009f555b75803e6cc3f9345cc272
GMS from home <537719@gmail.com>
2016-04-03 16:38:54 +0200

    remplace le goto debut initial par une mise en commentaire :: de tout le header

--- ratioruptures.cmd
+++ ratioruptures.cmd
@@ -1,11 +1,12 @@
+::ratioruptures.cmd
+::11/03/2016 11:34:25,14
+::affiche pour chaque entité le taux de dossiers ayant connus une rupture depuis le début du mois
+::suppose que l'on vienne de demander à GLPI d'exporter au format csv la stat de calcul d'autonomie
+::
+::MODIF 11:42 mardi 15 mars 2016 : n'applique plus de filtre de tri en sortie, le tri est désormais effectué au niveau du script awk.
+::MODIF 03/04/2016 16:30:44,69 remplace le goto debut initial par une mise en commentaire :: de tout le header
 @echo off
-goto :debut
-ratioruptures.cmd
-11/03/2016 11:34:25,14
-affiche pour chaque entité le taux de dossiers ayant connus une rupture depuis le début du mois
-suppose que l'on vienne de demander à GLPI d'exporter au format csv la stat de calcul d'autonomie
-
-MODIF 11:42 mardi 15 mars 2016 : n'applique plus de filtre de tri en sortie, le tri est désormais effectué au niveau du script awk.
+::goto :debut
 :debut
 dir /s /b /od glpi*.csv |tail -1 >%temp%\file.tmp
 set /p fichier=<%temp%\file.tmp
