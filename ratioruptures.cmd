2675d286414c10f8f81491ce4fc796043f4bec44
537719 <537719@gmail.com>
2016-04-29 11:13:43 +0200

    Added files via upload
    redirection de la sortie vers un fichier

--- ratioruptures.cmd
+++ ratioruptures.cmd
@@ -6,6 +6,7 @@ affiche pour chaque entité le taux de dossiers ayant connus une rupture depuis 
 suppose que l'on vienne de demander à GLPI d'exporter au format csv la stat de calcul d'autonomie
 
 MODIF 11:42 mardi 15 mars 2016 : n'applique plus de filtre de tri en sortie, le tri est désormais effectué au niveau du script awk.
+MODIF 11:11 vendredi 29 avril 2016 : redirige la sortie vers un fichier
 :debut
 dir /s /b /od glpi*.csv |tail -1 >%temp%\file.tmp
 set /p fichier=<%temp%\file.tmp
@@ -13,7 +14,7 @@ call filtrestat.cmd %fichier%
 dir /s /b /od glpi*.txt|tail -1 >%temp%\file.tmp
 set /p fichier=<%temp%\file.tmp
 REM gawk -f ratioruptures.awk %fichier% |sort /R
-gawk -f ratioruptures.awk %fichier% 
+gawk -f ratioruptures.awk %fichier% >ratioruptures.csv
 REM goto :eof
 set fichier=
 del %temp%\file.tmp
