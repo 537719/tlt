1ff1b6c0c809527058322976eb9fca89e2323c4a
537719 <537719@gmail.com>
2016-07-01 15:49:21 +0200

    Add files via upload
    Améliorations mineures

--- statscoli.cmd
+++ statscoli.cmd
@@ -11,13 +11,18 @@ PREREQUIS :
 	awk (ici dans sa version gnuwin32 : gawk)
 	script statscoli.awk
 	le plus récent des fichier glpi.csv situés dans l'arborescence en dessous du répertoire courant
+	udate (date unix recompilé windows et renommé)
+#MODIF 15:46 vendredi 1 juillet 2016 : Affiche en fin de traitement le numéro de la semaine en cours (pour inclusion dans le titre du fichier excel de stats)
 :debut
 dir /s /b /od glpi*.csv |tail -1 >%temp%\file.tmp
 set /p fichier=<%temp%\file.tmp
 call filtrestat.cmd %fichier%
 dir /s /b /od glpi*.txt|tail -1 >%temp%\file.tmp
 set /p fichier=<%temp%\file.tmp
-gawk -f ratioruptures.awk %fichier% |sort /R
+gawk -f statscoli.awk %fichier% >statscoli.csv
+REM statscoli.csv
 goto :eof
 set fichier=
 del %temp%\file.tmp
+@echo Nous sommes dans la semaine
+udate +%V
