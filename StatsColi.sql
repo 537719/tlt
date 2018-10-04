d78784753fb35b708ded7a5afcadfa2bbb34df5c
537719 <537719@gmail.com>
2018-10-04 10:24:53 +0200

    Correction du fait que certaines entités n'ayant aucune activité ne sortent pas au lieu de sortir à zéro

--- StatsColi.sql
+++ StatsColi.sql
@@ -5,14 +5,18 @@
 -- Usage : Invocation via SQLite3.exe avec un fichier GLPI.CSV accessible :
 -- sqlite3 < ../statscoli.sql
 -- CREATION 03/05/2018 - 16:00:38 première version opérationnelle, remplace la conjonction de scripts StatsColi.cmd et StatsColi.awk
--- MODIF 04/05/2018 - 10:50:43 rajout du commentaire relatif à la tenir à jour du chemin d'accès du fichier à importer
 
 -- DEBUT
 --  vide les anciennes données (inutile puisqu'on travaille sur une base temporaire
 -- DROP TABLE STATSCOLI;
 -- peuple la base avec les nouvelles données
+
+-- BUG 03/09/2018 - 10:15:41 non encore corrigé : le fait que certaines entités n'aient aucune activité fait qu'elles ne sortent pas au lieu de sortir à zéro
+--                          solution envisagée : créer une table intermédiaire initialisant à zéro toutes les entités dont on attend un résulat,
+--                                                l'alimenter avec le résultat du query actuel puis faire le query définitif dessus
+--     02/10/2018 - 11:42:28 implémentation de la correction en question
+
 .separator ;
--- la ligne suivante est réécrite par le script cmd appelant afin de tenir à jour le chemin d'accès au fichier
 .import C:/Users/Utilisateur/Documents/TLT/Stats/EnCours/glpi.csv STATSCOLI
 
 -- ajoute les champs nécessaires à la ventilation des stats à produire
@@ -137,7 +141,9 @@ AND (   TypeSite="SPECIFIQUE"
 )
 ;
 
-UPDATE STATSCOLI SET Activite = CHAR(183) || "Autres" -- 183 = caractère trié en dernier et dont l'affichage est acceptable
+-- UPDATE STATSCOLI SET Activite = CHAR(183) || "Autres" -- 183 = caractère trié en dernier et dont l'affichage est acceptable
+UPDATE STATSCOLI SET Activite = "Autres" -- 183 = caractère trié en dernier et dont l'affichage est acceptable
+-- MODIF 01/10/2018 - 16:24:54 suppression du caractères 183 pour cause de modification dans la présentation des résultats
 WHERE   `Type`="Incident"
 AND     Activite = "Autres"
 ;
@@ -145,155 +151,349 @@ AND     Activite = "Autres"
 
 -- Production des stats
 .output StatsColi.csv
--- Repartition des dossiers :"
-SELECT "Repartition des dossiers :", COUNT(`Type`)
-FROM STATSCOLI
-WHERE   Entité LIKE "%COLI%"
-AND     (
-        `Historique des attributions` LIKE "%CIL%"
-OR      `Historique des attributions` LIKE "%PLANIF%"
-OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
-OR      `Historique des attributions` LIKE "%DEPART%"
-OR      `Historique des attributions` LIKE "%ECONOCOM%"
-OR      `Historique des attributions` LIKE "%CRII%"
-OR      `Historique des attributions` LIKE "%OPS%"
-OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
+-- section totalement refondue suite au BUG 03/09/2018 - 10:15:41
+-- -- Repartition des dossiers :"
+-- SELECT "Repartition des dossiers :", COUNT(`Type`)
+-- FROM STATSCOLI
+-- WHERE   Entité LIKE "%COLI%"
+-- AND     (
+        -- `Historique des attributions` LIKE "%CIL%"
+-- OR      `Historique des attributions` LIKE "%PLANIF%"
+-- OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
+-- OR      `Historique des attributions` LIKE "%DEPART%"
+-- OR      `Historique des attributions` LIKE "%ECONOCOM%"
+-- OR      `Historique des attributions` LIKE "%CRII%"
+-- OR      `Historique des attributions` LIKE "%OPS%"
+-- OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
 -- "
-        )
-;
-SELECT `Type`, COUNT(`Type`)
-FROM STATSCOLI
-WHERE   Entité LIKE "%COLI%"
-AND     (
-        `Historique des attributions` LIKE "%CIL%"
-OR      `Historique des attributions` LIKE "%PLANIF%"
-OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
-OR      `Historique des attributions` LIKE "%DEPART%"
-OR      `Historique des attributions` LIKE "%ECONOCOM%"
-OR      `Historique des attributions` LIKE "%CRII%"
-OR      `Historique des attributions` LIKE "%OPS%"
-OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
+        -- )
+-- ;
+-- SELECT `Type`, COUNT(`Type`)
+-- FROM STATSCOLI
+-- WHERE   Entité LIKE "%COLI%"
+-- AND     (
+        -- `Historique des attributions` LIKE "%CIL%"
+-- OR      `Historique des attributions` LIKE "%PLANIF%"
+-- OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
+-- OR      `Historique des attributions` LIKE "%DEPART%"
+-- OR      `Historique des attributions` LIKE "%ECONOCOM%"
+-- OR      `Historique des attributions` LIKE "%CRII%"
+-- OR      `Historique des attributions` LIKE "%OPS%"
+-- OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
 -- "
-        )
-GROUP BY `Type`
-ORDER BY `Type` DESC
-;
-SELECT CHAR(13);
+--         -- )
+-- GROUP BY `Type`
+-- ORDER BY `Type` DESC
+-- ;
+-- SELECT CHAR(13);
+-- -- Stat des Incidents par type de site
+-- SELECT "Incidents par type de site", COUNT(`Type`)
+-- FROM STATSCOLI
+-- WHERE   Entité LIKE "%COLI%"
+-- AND `Type` = "Incident"
+-- AND     (
+        -- `Historique des attributions` LIKE "%CIL%"
+-- OR      `Historique des attributions` LIKE "%PLANIF%"
+-- OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
+-- OR      `Historique des attributions` LIKE "%DEPART%"
+-- OR      `Historique des attributions` LIKE "%ECONOCOM%"
+-- OR      `Historique des attributions` LIKE "%CRII%"
+-- OR      `Historique des attributions` LIKE "%OPS%"
+-- OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
+-- "
+--        -- )
+-- ;
+-- SELECT "TypeSite", COUNT(`Type`)
+-- FROM STATSCOLI
+-- WHERE   Entité LIKE "%COLI%"
+-- AND `Type` = "Incident"
+-- AND     (
+        -- `Historique des attributions` LIKE "%CIL%"
+-- OR      `Historique des attributions` LIKE "%PLANIF%"
+-- OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
+-- OR      `Historique des attributions` LIKE "%DEPART%"
+-- OR      `Historique des attributions` LIKE "%ECONOCOM%"
+-- OR      `Historique des attributions` LIKE "%CRII%"
+-- OR      `Historique des attributions` LIKE "%OPS%"
+-- OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
+-- "
+--        -- )
+-- GROUP BY TypeSite
+-- ORDER BY TypeSite
+-- ;
+-- SELECT CHAR(13);
+--
+-- -- Stat des Incidents par type d'activité
+-- SELECT "Incidents par type d'activité", COUNT(`Type`)
+-- FROM STATSCOLI
+-- WHERE   Entité LIKE "%COLI%"
+-- AND `Type` = "Incident"
+-- AND     (
+        -- `Historique des attributions` LIKE "%PLANIF%"
+-- OR      `Historique des attributions` LIKE "%CIL%"
+-- OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
+-- OR      `Historique des attributions` LIKE "%DEPART%"
+-- OR      `Historique des attributions` LIKE "%ECONOCOM%"
+-- OR      `Historique des attributions` LIKE "%CRII%"
+-- OR      `Historique des attributions` LIKE "%OPS%"
+-- OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
+-- "
+--        -- )
+-- -- ;
+--
+-- SELECT "Activite", COUNT(`Type`)
+-- FROM STATSCOLI
+-- WHERE   Entité LIKE "%COLI%"
+-- AND `Type` = "Incident"
+-- AND     (
+        -- `Historique des attributions` LIKE "%PLANIF%"
+-- OR      `Historique des attributions` LIKE "%CIL%"
+-- OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
+-- OR      `Historique des attributions` LIKE "%DEPART%"
+-- OR      `Historique des attributions` LIKE "%ECONOCOM%"
+-- OR      `Historique des attributions` LIKE "%CRII%"
+-- OR      `Historique des attributions` LIKE "%OPS%"
+-- OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
+-- "
+--        -- )
+-- GROUP BY Activite
+-- ORDER BY Activite
+-- ;
+-- SELECT CHAR(13);
+--
+-- -- Stat du Fil de l'eau PC par type de site
+-- SELECT "Fil de l'eau PC par type de site", COUNT(`Type`)
+-- FROM STATSCOLI
+-- WHERE   Entité LIKE "%COLI%"
+-- AND `Type` = "Demande"
+-- AND     (
+        -- `Historique des attributions` LIKE "%CIL%"
+-- OR      `Historique des attributions` LIKE "%PLANIF%"
+-- OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
+-- OR      `Historique des attributions` LIKE "%DEPART%"
+-- OR      `Historique des attributions` LIKE "%ECONOCOM%"
+-- OR      `Historique des attributions` LIKE "%CRII%"
+-- OR      `Historique des attributions` LIKE "%OPS%"
+-- OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
+-- "
+--        -- )
+-- AND Catégorie LIKE "%Demande de ma%ouveau poste de travail%PC%"
+-- ;
+-- SELECT "TypeSite", COUNT(`Type`)
+-- FROM STATSCOLI
+-- WHERE   Entité LIKE "%COLI%"
+-- AND `Type` = "Demande"
+-- AND     (
+        -- `Historique des attributions` LIKE "%CIL%"
+-- OR      `Historique des attributions` LIKE "%PLANIF%"
+-- OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
+-- OR      `Historique des attributions` LIKE "%DEPART%"
+-- OR      `Historique des attributions` LIKE "%ECONOCOM%"
+-- OR      `Historique des attributions` LIKE "%CRII%"
+-- OR      `Historique des attributions` LIKE "%OPS%"
+-- OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
+-- "
+--        -- )
+-- AND Catégorie LIKE "%Demande de ma%ouveau poste de travail%PC%"
+-- GROUP BY TypeSite
+-- ORDER BY TypeSite
+-- ;
+-- SELECT CHAR(13);
 
--- Stat des Incidents par type de site
-SELECT "Incidents par type de site", COUNT(`Type`)
-FROM STATSCOLI
-WHERE   Entité LIKE "%COLI%"
-AND `Type` = "Incident"
-AND     (
-        `Historique des attributions` LIKE "%CIL%"
-OR      `Historique des attributions` LIKE "%PLANIF%"
-OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
-OR      `Historique des attributions` LIKE "%DEPART%"
-OR      `Historique des attributions` LIKE "%ECONOCOM%"
-OR      `Historique des attributions` LIKE "%CRII%"
-OR      `Historique des attributions` LIKE "%OPS%"
-OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
--- "
-        )
-;
-SELECT "TypeSite", COUNT(`Type`)
-FROM STATSCOLI
-WHERE   Entité LIKE "%COLI%"
-AND `Type` = "Incident"
-AND     (
-        `Historique des attributions` LIKE "%CIL%"
-OR      `Historique des attributions` LIKE "%PLANIF%"
-OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
-OR      `Historique des attributions` LIKE "%DEPART%"
-OR      `Historique des attributions` LIKE "%ECONOCOM%"
-OR      `Historique des attributions` LIKE "%CRII%"
-OR      `Historique des attributions` LIKE "%OPS%"
-OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
--- "
-        )
-GROUP BY TypeSite
-ORDER BY TypeSite
-;
-SELECT CHAR(13);
-
---  Stat des Incidents par type d'activité
-SELECT "Incidents par type d'activité", COUNT(`Type`)
-FROM STATSCOLI
-WHERE   Entité LIKE "%COLI%"
-AND `Type` = "Incident"
-AND     (
-        `Historique des attributions` LIKE "%PLANIF%"
-OR      `Historique des attributions` LIKE "%CIL%"
-OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
-OR      `Historique des attributions` LIKE "%DEPART%"
-OR      `Historique des attributions` LIKE "%ECONOCOM%"
-OR      `Historique des attributions` LIKE "%CRII%"
-OR      `Historique des attributions` LIKE "%OPS%"
-OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
--- "
-        )
+-- nouvelle section de production des résultats suite au BUG 03/09/2018 - 10:15:41
+-- création de la table des résultats
+DROP TABLE IF EXISTS resultats ;
+CREATE TABLE resultats(
+    "stat" TEXT,
+    "rang" INTEGER,
+    "item" TEXT,
+    "nombre" INTEGER
+)
 ;
 
-SELECT "Activite", COUNT(`Type`)
-FROM STATSCOLI
-WHERE   Entité LIKE "%COLI%"
-AND `Type` = "Incident"
-AND     (
-        `Historique des attributions` LIKE "%PLANIF%"
-OR      `Historique des attributions` LIKE "%CIL%"
-OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
-OR      `Historique des attributions` LIKE "%DEPART%"
-OR      `Historique des attributions` LIKE "%ECONOCOM%"
-OR      `Historique des attributions` LIKE "%CRII%"
-OR      `Historique des attributions` LIKE "%OPS%"
-OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
--- "
-        )
-GROUP BY Activite
-ORDER BY Activite
+-- initialisation à zéro de tous les résultats de la table
+INSERT INTO resultats VALUES(
+    "site",1,"ACP",0
+),(
+    "site",2,"CLIENT",0
+),(
+    "site",3,"DOT",0
+),(
+    "site",4,"PFC",0
+),(
+    "site",5,"SIEGE/SAV",0
+),(
+    "site",6,"SOUS-TRAITANT",0
+),(
+    "site",7,"SPECIFIQUE",0
+),(
+    "type",1,"Incident",0
+),(
+    "type",2,"Demande",0
+),(
+    "FDE",1,"ACP",0
+),(
+    "FDE",2,"PFC",0
+),(
+    "FDE",3,"ADM",0
+),(
+    "activite",1,"Expeditor",0
+),(
+    "activite",2,"Imprimante",0
+),(
+    "activite",3,"UC Fixe",0
+),(
+    "activite",4,"UC Portable",0
+),(
+    "activite",5,"Autres",0
+)
 ;
-SELECT CHAR(13);
+
+-- SELECT * FROM resultats
+-- ORDER BY stat
+-- -- GROUP BY stat
+-- ;
+
+-- actualisation des résultats pour la répartition des dossiers :"
+WITH storage AS (
+    SELECT
+        Type AS TypeDossier ,COUNT(Type) AS NbType
+    FROM STATSCOLI
+    WHERE Entité LIKE "%COLI%"
+    -- AND Type = "Incident"
+    AND     (
+            `Historique des attributions` LIKE "%CIL%"
+        OR  `Historique des attributions` LIKE "%PLANIF%"
+        OR  `Historique des attributions` LIKE "%SOLUTIONS30%"
+        OR  `Historique des attributions` LIKE "%DEPART%"
+        OR  `Historique des attributions` LIKE "%ECONOCOM%"
+        OR  `Historique des attributions` LIKE "%CRII%"
+        OR  `Historique des attributions` LIKE "%OPS%"
+        OR  `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
+    -- " double quote en commentaire afin que la coloration syntaxique retombe sur ses pieds après le ESCAPE
+        )
+    GROUP BY TypeDossier
+)
+
+UPDATE resultats
+SET
+    nombre=(
+        SELECT NbType FROM storage WHERE
+        storage.TypeDossier=resultats.item -- AND resultats.stat="type"
+)
+WHERE resultats.stat="type"
+;
+
+-- actualisation des résultats pour les incidents par type de site
+WITH storage AS (
+    SELECT
+        TypeSite,COUNT(Type) AS NbSite
+    FROM STATSCOLI
+    WHERE Entité LIKE "%COLI%"
+    AND Type = "Incident"
+    AND     (
+            `Historique des attributions` LIKE "%CIL%"
+        OR  `Historique des attributions` LIKE "%PLANIF%"
+        OR  `Historique des attributions` LIKE "%SOLUTIONS30%"
+        OR  `Historique des attributions` LIKE "%DEPART%"
+        OR  `Historique des attributions` LIKE "%ECONOCOM%"
+        OR  `Historique des attributions` LIKE "%CRII%"
+        OR  `Historique des attributions` LIKE "%OPS%"
+        OR  `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
+    -- " double quote en commentaire afin que la coloration syntaxique retombe sur ses pieds après le ESCAPE
+        )
+    GROUP BY TypeSite
+)
+
+UPDATE resultats
+SET
+    nombre=(
+        SELECT NbSite FROM storage WHERE
+        storage.TypeSite=resultats.item -- AND resultats.stat="site"
+)
+WHERE resultats.stat="site"
+;
+
+-- actualisation des résultats pour les incidents par type d'activité
+WITH storage AS (
+    SELECT
+        Activite,COUNT(Type) AS NbActi
+    FROM STATSCOLI
+    WHERE Entité LIKE "%COLI%"
+    AND Type = "Incident"
+    AND     (
+            `Historique des attributions` LIKE "%CIL%"
+        OR  `Historique des attributions` LIKE "%PLANIF%"
+        OR  `Historique des attributions` LIKE "%SOLUTIONS30%"
+        OR  `Historique des attributions` LIKE "%DEPART%"
+        OR  `Historique des attributions` LIKE "%ECONOCOM%"
+        OR  `Historique des attributions` LIKE "%CRII%"
+        OR  `Historique des attributions` LIKE "%OPS%"
+        OR  `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
+    -- " double quote en commentaire afin que la coloration syntaxique retombe sur ses pieds après le ESCAPE
+        )
+    GROUP BY Activite
+)
+
+UPDATE resultats
+SET
+    nombre=(
+        SELECT NbActi FROM storage WHERE
+        storage.Activite=resultats.item -- AND resultats.stat="activite"
+)
+WHERE resultats.stat="activite"
+;
+
+-- actualisation des résultats pour le Fil de l'eau PC par type de site
+ WITH storage AS (
+   SELECT
+        TypeSite,COUNT(Type) AS NbSite
+    FROM STATSCOLI
+    WHERE Entité LIKE "%COLI%"
+    AND Type = "Demande"
+    AND     (
+            `Historique des attributions` LIKE "%CIL%"
+        OR  `Historique des attributions` LIKE "%PLANIF%"
+        OR  `Historique des attributions` LIKE "%SOLUTIONS30%"
+        OR  `Historique des attributions` LIKE "%DEPART%"
+        OR  `Historique des attributions` LIKE "%ECONOCOM%"
+        OR  `Historique des attributions` LIKE "%CRII%"
+        OR  `Historique des attributions` LIKE "%OPS%"
+        OR  `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
+    -- " double quote en commentaire afin que la coloration syntaxique retombe sur ses pieds après le ESCAPE
+        )
+    AND Catégorie LIKE "%Demande de ma%ouveau poste de travail%PC%"
+    GROUP BY TypeSite
+)
+
+UPDATE resultats
+SET
+    nombre=(
+        SELECT NbSite FROM storage WHERE
+        storage.TypeSite=resultats.item
+)
+WHERE resultats.stat="FDE"
+;
 
 
--- Stat du Fil de l'eau PC par type de site
-SELECT "Fil de l'eau PC par type de site", COUNT(`Type`)
-FROM STATSCOLI
-WHERE   Entité LIKE "%COLI%"
-AND `Type` = "Demande"
-AND     (
-        `Historique des attributions` LIKE "%CIL%"
-OR      `Historique des attributions` LIKE "%PLANIF%"
-OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
-OR      `Historique des attributions` LIKE "%DEPART%"
-OR      `Historique des attributions` LIKE "%ECONOCOM%"
-OR      `Historique des attributions` LIKE "%CRII%"
-OR      `Historique des attributions` LIKE "%OPS%"
-OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
--- "
-        )
-AND Catégorie LIKE "%Demande de ma%ouveau poste de travail%PC%"
-;
-SELECT "TypeSite", COUNT(`Type`)
-FROM STATSCOLI
-WHERE   Entité LIKE "%COLI%"
-AND `Type` = "Demande"
-AND     (
-        `Historique des attributions` LIKE "%CIL%"
-OR      `Historique des attributions` LIKE "%PLANIF%"
-OR      `Historique des attributions` LIKE "%SOLUTIONS30%"
-OR      `Historique des attributions` LIKE "%DEPART%"
-OR      `Historique des attributions` LIKE "%ECONOCOM%"
-OR      `Historique des attributions` LIKE "%CRII%"
-OR      `Historique des attributions` LIKE "%OPS%"
-OR      `Historique des attributions` LIKE "%I\&amp;S%" ESCAPE "\"
--- "
-        )
-AND Catégorie LIKE "%Demande de ma%ouveau poste de travail%PC%"
-GROUP BY TypeSite
-ORDER BY TypeSite
-;
-SELECT CHAR(13);
+-- correction à zéro des résultats nuls
+UPDATE resultats SET nombre=0 WHERE nombre IS NULL ;
+
+
+-- affichage des résultats
+-- select stat,item,nombre from resultats  order by stat,rang;
+SELECT "Repartition des dossiers :", SUM(`nombre`) FROM resultats WHERE stat="type";
+SELECT item, nombre FROM resultats WHERE stat="type" ORDER BY rang;
+
+SELECT "Incidents par type de site :", SUM(`nombre`) FROM resultats WHERE stat="site";
+SELECT item, nombre FROM resultats WHERE stat="site" ORDER BY rang;
+
+SELECT "Incidents par type d'activité :", SUM(`nombre`) FROM resultats WHERE stat="activite";
+SELECT item, nombre FROM resultats WHERE stat="activite" ORDER BY rang;
+
+SELECT "Fil de l'eau PC par type de site :", SUM(`nombre`) FROM resultats WHERE stat="FDE";
+SELECT item, nombre FROM resultats WHERE stat="FDE" ORDER BY rang;
+
 
 -- Incidents par catégorie
 SELECT "Incidents par catégorie", COUNT(`Type`)
@@ -337,4 +537,4 @@ SELECT CHAR(13);
 
 -- Et c'est fini
 SELECT "Résultat dans le fichier StatsColi.csv" ;
-.exit
+-- .exit
